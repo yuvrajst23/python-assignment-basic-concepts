88090f3b094d67ad386906d9f84183fe79639d6d
yuvrajst23 <yuvraj.st23@gmail.com>
2025-06-27 00:25:07 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,28 @@
+#Task 1
+#Two Number Input from the User
+num1 = float(input("Enter the First Number: "))
+num2 = float(input("Enter the Second Number: "))
+
+#Perform Basic Operation
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1/num2
+
+#Results
+print("Addition", addition)
+print("Subtraction", subtraction)
+print("Multiplication", multiplication)
+print("Division", division)
+
+#Task 2
+#Taking first and last name as input
+first_name = input("Enter Your First name: ")
+last_name = input("Enter Your Last name: ")
+
+# Concatenates the first name and the last name into a full name
+full_name = first_name + " " + last_name
+
+#Prints a personalized greeting message using the full name
+print("Hello", full_name + "! Welcome to Python Program")
+
